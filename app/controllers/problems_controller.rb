c3f0f4bacae2b679a31dbe7e6ac58d22077ed59c
Ikuo Terado <eqobar@gmail.com>
2014-09-21 02:51:29 +0900

    Support editting problem

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -19,6 +19,19 @@ class ProblemsController < ApplicationController
     @problem = Problem.find(params[:id])
   end
 
+  def edit
+    @problem = current_user.submitted_problems.find(params[:id])
+  end
+
+  def update
+    @problem = current_user.submitted_problems.find(params[:id])
+    if @problem.update(problem_params)
+      redirect_to @problem, notice: 'Updated'
+    else
+      render :edit
+    end
+  end
+
   private
 
   def problem_params
