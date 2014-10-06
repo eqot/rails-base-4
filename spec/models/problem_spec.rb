205a5a4f972597ff9a233ebe84a38933045ec2dd
Ikuo Terado <eqobar@gmail.com>
2014-10-07 02:00:49 +0900

    Add a few tests for problem

--- spec/models/problem_spec.rb
+++ spec/models/problem_spec.rb
@@ -6,4 +6,15 @@ describe Problem do
     expect(build(:problem)).to be_valid
   end
 
+  it "is invalid without a title" do
+    problem = build(:problem, title: nil)
+    expect(problem).to have(1).errors_on(:title)
+  end
+
+  it "checks if valid user is submitted" do
+    user = build(:user)
+    problem = build(:problem, owner_id: user)
+    problem.submitted_by?(user).should be_truthy
+  end
+
 end
