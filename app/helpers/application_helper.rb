2c56340c89de2a376dafefc40e89db9044248e2a
Ikuo Terado <eqobar@gmail.com>
2014-10-20 03:04:28 +0900

    Polish code for flash message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,13 @@ module ApplicationHelper
     content_tag(:abbr, time.to_s, options.merge(:title => time.getutc.iso8601)) if time
   end
 
+  def bootstrap_class_for(level)
+    case level
+      when "success" then "alert-success"
+      when "notice"  then "alert-info"
+      when "alert"   then "alert-warning"
+      when "error"   then "alert-danger"
+    end
+  end
+
 end
